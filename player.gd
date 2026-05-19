4df8afe05c85a27985b3ac15982e0ec72be4325d
EstebanG17-ia <estradaesteban2000@gmail.com>
2026-05-18 23:41:02 -0700

    Tercer commit movimiento de camara

--- player.gd
+++ player.gd
@@ -2,29 +2,41 @@ extends RigidBody3D
 
 @onready var spring_arm_3d: SpringArm3D = $SpringArm3D
 @onready var camera_3d: Camera3D = $SpringArm3D/Camera3D
-var speed = 0.5
-var mouse = 1	
 
-func input(event: InputEvent):
+var speed = 0.5
+var mouse = 0.01
+
+func _input(event: InputEvent):
 	if event is InputEventMouseMotion:
 		spring_arm_3d.rotation.x -= event.relative.y * mouse
 		spring_arm_3d.rotation.y -= event.relative.x * mouse
-		#spring_arm_3d.rotation.x = clamp(spring_arm_3d.rotation.x, -PI/2, PI/4)
-		spring_arm_3d.rotation.x = clamp(spring_arm_3d.rotation.x, deg_to_rad(-60), PI/4)
+		
+		spring_arm_3d.rotation.x = clamp(
+			spring_arm_3d.rotation.x,
+			deg_to_rad(-60),
+			PI / 4
+		)
 
 func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	spring_arm_3d.top_level = true
 	spring_arm_3d.position = Vector3(0, 3, 0)
 
 func _process(delta: float) -> void:
-	spring_arm_3d.global_position= self.global_position
-	
+	spring_arm_3d.global_position = self.global_position
+
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	var inputDirection = Vector3(
-		Input.get_axis("left","right"),
+		Input.get_axis("left", "right"),
 		0,
-		Input.get_axis("up","down")
+		Input.get_axis("up", "down")
 	)
-	inputDirection = inputDirection.rotated(Vector3.UP, spring_arm_3d.rotation.y).normalized() * speed
-	
-	apply_central_impulse(Vector3(inputDirection.x, 0, inputDirection.z))
+
+	inputDirection = inputDirection.rotated(
+		Vector3.UP,
+		spring_arm_3d.rotation.y
+	).normalized() * speed
+
+	apply_central_impulse(
+		Vector3(inputDirection.x, 0, inputDirection.z)
+	)
